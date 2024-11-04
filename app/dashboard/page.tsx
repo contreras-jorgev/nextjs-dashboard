492fc8795ad49ef5bfe7970f5da738c86265cfb7
Jorge Contreras <jcontreras@asna.com>
2024-11-04 11:17:30 -0600

    More Scheletons

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,15 +3,16 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { // fetchRevenue, 
-    fetchLatestInvoices,
+    // fetchLatestInvoices,
     fetchCardData
 } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
     // const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
+    // const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
     const {
       numberOfInvoices,
       numberOfCustomers,
@@ -38,7 +39,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
